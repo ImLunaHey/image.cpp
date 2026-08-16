8cf146cbcfed124f8b5b4ac2ddd110291de84851
luna <luna@wvvw.me>
2026-08-16 17:04:46 +0100

    fix(build): link shared Windows dependencies

---
+++ src/provider/leptonica_no_subprocess.h
@@ -0,0 +1,18 @@
+#ifndef IMAGECPP_LEPTONICA_NO_SUBPROCESS_H
+#define IMAGECPP_LEPTONICA_NO_SUBPROCESS_H
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int imagecpp_leptonica_system_disabled(const char *command);
+
+#ifdef __cplusplus
+}
+#endif
+
+#define system imagecpp_leptonica_system_disabled
+
+#endif
